bd995ef356e0ccaeb878884472c0462f99063f86
ZakariyeAden <zakariye5417@gmail.com>
2023-07-07 10:42:58 -0500

    adding images and routes work in Postman

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,46 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+  {
+    id: 1,
+    path: "images/goat_small.jpg",
+    title: "Goat",
+    description: "Photo of a goat taken at Glacier National Park.",
+    likes: 0,
+  },
+  {
+    id: 2,
+    path: "images/Egypt.jpg",
+    title:"Egypt",
+    description: "Location in Egypt.",
+    likes: 0,
+  },
+   {
+    id: 3,
+    path: "images/Mecca.jpg",
+    title:"Egypt",
+    description: "Location in Mecca.",
+    likes: 0,
+  },
+  {
+    id: 4,
+    path: "images/Medina.jpg",
+    title:"Medina",
+    description: "Location in Medina.",
+    likes: 0,
+  },
+  {
+    id: 5,
+    path: "images/Palestine.jpg",
+    title:"Medina",
+    description: "Location in Palestine.",
+    likes: 0,
+  },
+  {
+    id: 6,
+    path: "images/Somalia.jpg",
+    title:"Medina",
+    description: "Location in Somalia.",
+    likes: 0,
+  },
 ];
 
 module.exports = galleryItems;
