5afd1a15472779028c078c65561fd1727f7e3ac4
ZakariyeAden <zakariye5417@gmail.com>
2023-07-08 16:58:52 -0500

    likes work but need to fix incrementing now

--- database.sql
+++ database.sql
@@ -2,7 +2,7 @@ CREATE TABLE "gallery" (
 	"id" SERIAL PRIMARY KEY,
 	"image" varchar(255) NOT NULL,
 	"title" varchar(80) NOT NULL,
-	"description" varchar(80) NOT NULL,
+	"description" varchar(255) NOT NULL,
 	"likes"  INT DEFAULT 0
 );
 
@@ -12,6 +12,7 @@ INSERT INTO gallery ("image", "title", "description")
 VALUES 
 ('images/goat_small.jpg', 'Goat', 'Photo of a goat taken at Glacier National Park.'),
 ('images/Egypt.jpg', 'Egypt', 'Location in Egypt, have good tea and best place to study.'),
+('images/Somalia.jpg', 'Somalia', 'Location in Somalia, Liddo Beach is beautiful place to visit!'),
+('images/Palestine.jpg', 'Palestine', 'Location in Palestine, Al Gaza, One prayer in Masjid al Aqsa is equal to 500!'),
 ('images/Mecca.jpg', 'Mecca', 'One prayer in Mecca is equal to 100,000!'),
-('images/Medina.jpg', 'Medina', 'The Prophets Masjid.'),
-('images/Somalia.jpg', 'Somalia', 'Location in Somalia, Liddo Beach is beautiful place to visit!');
+('images/Medina.jpg', 'Medina', 'The Prophets (peace and blessing be upon on him) Masjid. One prayer is equal to 1,000!');
--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,22 +1,36 @@
 const express = require('express');
 const router = express.Router();
-const galleryItems = require('../modules/gallery.data');
+
 const pool = require('../modules/pool');
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
 // PUT Route
+// router.put('/like/:id', (req, res) => {
+//     console.log(req.params);
+//     const galleryId = req.params.id;
+//     for(const galleryItem of galleryItems) {
+//         if(galleryItem.id == galleryId) {
+//             galleryItem.likes += 1;
+//         }
+//     }
+//     res.sendStatus(200);
+// }); // END PUT Route
+// PUT Route for Likes
 router.put('/like/:id', (req, res) => {
-    console.log(req.params);
     const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
-}); // END PUT Route
-
+    // Need to figure out how to increment everytime. Could this issue be in setting up the table or add += 1 in Client?
+    const sqlText = `
+    UPDATE gallery SET "likes" = 1 WHERE id = $1;
+    `
+    pool.query(sqlText, [galleryId])
+    .then((result) => {
+      res.sendStatus(200)
+    }).catch((err) => {
+      alert('Error Up')
+      res.sendStatus(500)
+    });
+  })
 // GET Route
 router.get('/', (req, res) => {
     let sqlList = 'SELECT * FROM "gallery";';
